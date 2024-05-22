e2eb7edb9b796a488704c8f4a81866c1f6f4ed99
unknown <mahdi.zrei@gmail.com>
2024-05-22 15:33:39 +0330

    remove deadline wrapper

--- hysteria2/service_packet.go
+++ hysteria2/service_packet.go
@@ -3,7 +3,6 @@ package hysteria2
 import (
 	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/auth"
-	"github.com/sagernet/sing/common/canceler"
 	E "github.com/sagernet/sing/common/exceptions"
 	M "github.com/sagernet/sing/common/metadata"
 )
@@ -48,8 +47,7 @@ func (s *serverSession[U]) handleUDPMessage(message *udpMessage) {
 		s.udpAccess.Lock()
 		s.udpConnMap[message.sessionID] = udpConn
 		s.udpAccess.Unlock()
-		newCtx, newConn := canceler.NewPacketConn(udpConn.ctx, udpConn, s.udpTimeout)
-		go s.handler.NewPacketConnection(newCtx, newConn, M.Metadata{
+		go s.handler.NewPacketConnection(udpConn.ctx, udpConn, M.Metadata{
 			Source:      s.source,
 			Destination: M.ParseSocksaddr(message.destination),
 		})
